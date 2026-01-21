39b380409459a6d7272ca91dd0ac947f6b20da29
M-shahroz227 <shahrozkhan646411@gmail.com>
2026-01-21 08:18:05 +0500

    update...

--- onlineStore/Service/RegisterService/RegisterService.cs
+++ onlineStore/Service/RegisterService/RegisterService.cs
@@ -1,6 +1,29 @@
-﻿namespace onlineStore.Service.RegisterService
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using onlineStore.Data;
+using onlineStore.Model;
+
+namespace onlineStore.Service.RegisterService
 {
     public class RegisterService : IRegisterService
     {
+        private  readonly StoreDbContext _context;
+        public RegisterService(StoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Register> RegisterUser(Register register)
+        {
+            var newUser = new Register
+            {
+                Username = register.Username,
+                Password = register.Password,
+                Email = register.Email,
+                PhoneNumber = register.PhoneNumber
+            };
+            _context.registers.Add(newUser);
+            await _context.SaveChangesAsync();
+            return newUser;
+
+        }
     }
 }
